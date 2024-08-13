4e42c47fb7218832f8dde1bebb11610a644a195c
Braian <braian.gonzales77@gmail.com>
2024-08-13 20:25:53 +0200

    rename in header

--- components/header.js
+++ components/header.js
@@ -22,7 +22,7 @@ const Header = ({ currentUser }) => {
   return (
     <nav className="navbar navbar-light bg-light ps-2">
       <Link href="/">
-        <a className="navbar-brand">GitTix</a>
+        <a className="navbar-brand">Ticketing</a>
       </Link>
 
       <div className="d-flex justify-content-end">
